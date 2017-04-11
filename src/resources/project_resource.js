1a4baf989ae35041c67a5bea24025b20e373f65d
Michael Franzkowiak <michael@contiamo.com>
2017-04-11 10:36:52 +0200

    Add notebook and presentation resource.

--- src/resources/project_resource.js
+++ src/resources/project_resource.js
@@ -2,6 +2,8 @@
 import {MakeResource} from './shared/make_resource';
 import AppResource from './app_resource';
 import DashboardResource from './dashboard_resource';
+import NotebookResource from './notebook_resource';
+import PresentationResource from './presentation_resource';
 
 const capabilities = [
   'Creatable',
@@ -15,11 +17,15 @@ export default function(parent: Object, path: string) {
   return class ProjectResource extends MakeResource(parent, path, capabilities) {
     app: any;
     dashboard: any;
+    notebook: any;
+    presentation: any;
 
     constructor(...args: any) {
       super(...args);
       this.app = AppResource(this, 'apps');
       this.dashboard = DashboardResource(this, 'dashboards');
+      this.notebook = NotebookResource(this, 'notebooks');
+      this.presentation = PresentationResource(this, 'presentations');
     }
 
     query(attrs: Object = {}) {
